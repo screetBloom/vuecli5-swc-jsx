28e58fa61bd08c5f9ecf6b27eac9888c43cf7bde
screetBloom <15306225869@163.com>
2022-06-01 18:46:56 +0800

    feat: 增加边界case

--- src/components/jsx.vue
+++ src/components/jsx.vue
@@ -2,12 +2,16 @@
 export default {
   props: ['msg'],
   render() {
+    const item = {name: '测试'}
+
+    // 当 {...{}} 后还有其它属性时会报错: Module not found: Error: Can't resolve 'react' 
     return (
       <div>
-        <br/>
+        <br />
         <p>JSX子组件</p>
         <p>msg: {this.msg}</p>
-        <br/>
+        <br />
+        <div {...{ props: {} }} key={item.name}>测试</div>
       </div>
     )
   },

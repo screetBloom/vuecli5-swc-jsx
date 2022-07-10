d955dfc1b8e820d0abb8292850e425ff22577d2f
screetBloom <15306225869@163.com>
2022-07-11 01:55:06 +0800

    feat: 增加复现

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,29 @@
 module.exports = {
   chainWebpack: (config) => {
+    const opts = {
+      jsc: {
+        parser: {
+          syntax: 'ecmascript',
+          jsx: true,
+          dynamicImport: true,
+        },
+        transform: {
+          react: {
+            // "pragma": "h"
+            runtime: 'automatic',
+            importSource: '@lancercomet/vue2-jsx-runtime',
+            throwIfNamespace: false,
+          },
+        },
+      },
+    }
+
     config.module
       .rule('js')
       .uses.clear()
       .end()
       .use('swc-loader')
       .loader('swc-loader')
+      .options(opts)
   },
 }
